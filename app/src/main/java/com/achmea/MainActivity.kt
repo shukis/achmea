4a9fb296cf893ba967622dd183c5fd2546168b90
Pavel <pavel.shukis@outlook.com>
2025-04-24 13:45:11 +0300

    Add navigation and app icon

--- app/src/main/java/com/achmea/MainActivity.kt
+++ app/src/main/java/com/achmea/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.achmea.core.Route
 import com.achmea.designsystem.MaterialTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +23,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             MaterialTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    NavHost(navController, startDestination = Route.EmployersList) {
+                        composable<Route.EmployersList> {
+                            Text("EmployersList", modifier = Modifier
+                                .clickable {
+                                    navController.navigate(Route.EmployerDetails(""))
+                                }
+                                .padding(innerPadding))
+                        }
+                        composable<Route.EmployerDetails> {
+                            Text("EmployerDetails", modifier = Modifier.padding(innerPadding))
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MaterialTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/achmea/core/Route.kt
@@ -0,0 +1,10 @@
+package com.achmea.core
+
+import kotlinx.serialization.Serializable
+
+sealed interface Route {
+    @Serializable
+    data object EmployersList : Route
+    @Serializable
+    data class EmployerDetails(val id: String) : Route
+}
